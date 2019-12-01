e4f1efdea84943d8381f86ff8d8ac69d680b3c1b
Alexander Hultnér <ahultner@gmail.com>
2019-12-01 18:42:07 +0100

    Improved docstrings, added more type annotations

--- aoc/day_01/core.py
+++ aoc/day_01/core.py
@@ -12,39 +12,18 @@ the first. Each puzzle grants one star. Good luck!
 
 The Elves quickly load you into a spacecraft and prepare to launch.
 
-At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper.
-They haven't determined the amount of fuel required yet.
-
-Fuel required to launch a given module is based on its mass. Specifically, to
-find the fuel required for a module, take its mass, divide by three, round
-down, and subtract 2.
-
-For example:
-
- - For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to
-   get 2.
- - For a mass of 14, dividing by 3 and rounding down still yields 4, so the
-   fuel required is also 2.
- - For a mass of 1969, the fuel required is 654.
- - For a mass of 100756, the fuel required is 33583.
-The Fuel Counter-Upper needs to know the total fuel requirement.
-To find it, individually calculate the fuel needed for the mass of each module
-(your puzzle input), then add together all the fuel values.
-
-What is the sum of the fuel requirements for all of the modules on your
-spacecraft?
-
 """
 from numbers import Number
 from aoc.day_01.seed import p1
 
 
-def launch_fuel_required(mass: Number):
-    "Calculates the fuel required for given mass"
+def launch_fuel_required(mass: Number) -> Number:
+    "Returns the fuel required for given mass"
     return int(mass / 3) - 2
 
 
-def launch_fuel_for_fuel(mass):
+def launch_fuel_for_fuel(mass: Number) -> Number:
+    "Returns fuel required including fuel for the fuel mass"
     fuel = launch_fuel_required(mass)
     # Base case
     if fuel <= 0:
@@ -52,11 +31,35 @@ def launch_fuel_for_fuel(mass):
     return fuel + launch_fuel_for_fuel(fuel)
 
 
-def part_1(puzzle_input=p1):
+def part_1(puzzle_input: Tuple[Number] = p1) -> Number:
+    """
+    At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper.
+    They haven't determined the amount of fuel required yet.
+
+    Fuel required to launch a given module is based on its mass. Specifically,
+    to find the fuel required for a module, take its mass, divide by three,
+    round down, and subtract 2.
+
+    For example:
+
+     - For a mass of 12, divide by 3 and round down to get 4, then subtract 2
+       to get 2.
+     - For a mass of 14, dividing by 3 and rounding down still yields 4, so the
+       fuel required is also 2.
+     - For a mass of 1969, the fuel required is 654.
+     - For a mass of 100756, the fuel required is 33583.
+    The Fuel Counter-Upper needs to know the total fuel requirement.
+    To find it, individually calculate the fuel needed for the mass of each
+    module (your puzzle input), then add together all the fuel values.
+
+    What is the sum of the fuel requirements for all of the modules on your
+    spacecraft?
+
+    """
     return sum(launch_fuel_required(mass) for mass in puzzle_input)
 
 
-def part_2(puzzle_input=p1):
+def part_2(puzzle_input: Tuple[Number] = p1) -> Number:
     """
     --- Part Two ---
 
