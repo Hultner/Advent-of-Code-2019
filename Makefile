f4cff9cf5330e4e85df6729bdaff088b49ba8b34
Alexander Hultnér <ahultner@gmail.com>
2019-12-03 20:30:56 +0100

    Added tests to day bootstrapper

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 .PHONY : day
 day: 
 	cp -r aoc/day_0X aoc/day_`date +%d`
+	cp tests/test_day_0X.py tests/test_day_0X.py
---
+++ tests/test_day_0X.py
@@ -0,0 +1,24 @@
+from aoc.day_03.core import (
+    part_1,
+    part_2,
+)
+
+
+def test_parts():
+    # Oracle says so
+    # assert part_1() == 9581917
+    # assert part_2() == 2505
+    assert True
+
+
+def verify_x(data, expected):
+    assert True
+
+
+def test_x():
+    """
+    """
+    examples = ((0,1))
+
+    for (data, expected) in examples:
+        verify_x(data, expected)
